d81e841f08a7314edc2de108b9e46bfe408bd80c
EmilianoCabral <emicabral95@gmail.com>
2024-08-08 15:39:37 -0300

    adding logic to calculator

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 let screen = document.getElementById('screen');
 const buttons = document.querySelectorAll('#buttons a');
 
-for (const buttons of buttons) {
+for (const button of buttons) {
     button.addEventListener('click', function (e) {
-        e.prventDefault();
-    })
+        e.preventDefault();
+
+        if (e.target.dataset.key == 'equal') {
+            try {
+                let result = eval(screen.textContent);
+                screen.textContent = result;
+
+            if (screen.textContent.length > 8) {
+                screen.textContent = result.toFixed(8);
+            }
+            } catch (error) {
+                screen.textContent = 'error';
+            }
+            
+        } else if (e.target.dataset.key == 'clear') {
+            screen.textContent = '';
+        } else {
+            screen.textContent += e.target.dataset.key;
+        }
+    });
     
 }
 
