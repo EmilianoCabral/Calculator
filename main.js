9c8dd3ad4a4a93e0348c9a1409b6dc6de484eea0
EmilianoCabral <emicabral95@gmail.com>
2024-08-08 12:22:47 -0300

    Calculator

---
+++ main.js
@@ -0,0 +1,10 @@
+let screen = document.getElementById('screen');
+const buttons = document.querySelectorAll('#buttons a');
+
+for (const buttons of buttons) {
+    button.addEventListener('click', function (e) {
+        e.prventDefault();
+    })
+    
+}
+
